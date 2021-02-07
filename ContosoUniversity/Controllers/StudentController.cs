8c0fc272addf84198c81df414ecf54197a25df9c
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-02-07 22:39:01 +1000

    Добавлен вывод списка студентов

---
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var studentsList = await _context.Students.ToListAsync();
+            return View(studentsList);
+        }
+    }
+}
